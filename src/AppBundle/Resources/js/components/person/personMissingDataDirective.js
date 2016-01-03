259718395713eecf134df8fbeeebf286f0f84118
samisalami <kontakt@samisalami.de>
2016-01-03 21:45:43 +0100

    fixes 0.1.2

--- src/AppBundle/Resources/js/components/person/personMissingDataDirective.js
+++ src/AppBundle/Resources/js/components/person/personMissingDataDirective.js
@@ -1,12 +1,43 @@
 angular.module('app')
-    .directive('personMissingData',['personService', function(personService) {
+    .directive('personMissingData',['personService', 'mapmarkerService', function(personService, mapmarkerService) {
         return {
             restrict: 'E',
             templateUrl: 'templates/personMissingDataView.html',
             replace: true,
             link: function($scope, $element,$attrs) {
-                personService.getPersons(function (response) {
+                var personDataLoaded = false;
+                var mapmarkerDataLoaded = false;
+
+                $scope.dataLoaded = function(){
+                    return personDataLoaded && mapmarkerDataLoaded;
+                };
+
+                mapmarkerService.getMapmarkers(function (response) {
+                    $scope.mapmarkers = response;
+                    mapmarkerDataLoaded = true;
+                });
+
+                personService.getPersons(function(response){
                     $scope.persons = response;
+                    personDataLoaded = true;
+                });
+
+                $scope.$watch($scope.dataLoaded, function(dataLoaded){
+                    if(dataLoaded) {
+                        $scope.combinedPersons = [];
+                        var pcount = $scope.persons.length;
+                        var mcount = $scope.mapmarkers.length;
+                        for (var i=0; i<pcount; i++) {
+                            $scope.combinedPersons[i] = $scope.persons[i];
+                            $scope.combinedPersons[i]['hasMapmarker'] = false;
+                            for(var x=0;x<mcount;x++) {
+                                if($scope.mapmarkers[x]['person']['id'] == $scope.persons[i]['id']) {
+                                    $scope.combinedPersons[i]['hasMapmarker'] = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 });
             }
         }
