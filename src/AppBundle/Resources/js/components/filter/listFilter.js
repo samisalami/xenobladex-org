a5bf8d8177d0fdac55ed12e8d0e35b8212c2b82b
samisalami <kontakt@samisalami.de>
2016-01-17 05:41:29 +0100

    FIX: list filter

--- src/AppBundle/Resources/js/components/filter/listFilter.js
+++ src/AppBundle/Resources/js/components/filter/listFilter.js
@@ -4,27 +4,31 @@ angular.module('app')
     .filter('listFilter', function(){
         return function (list,filterValue,fieldname,childFieldname,returnEmpyList) {
             var result = [];
-            if(list && (filterValue || !returnEmpyList)) {
-                var count = list.length;
-                for (var i=0; i<count; i++) {
-                    var listValue = list[i];
-                    if(typeof fieldname !== 'undefined') {
-                        listValue = list[i][fieldname];
-                    }
+            if(!returnEmpyList && !filterValue) {
+                return list;
+            } else {
+                if(list && filterValue) {
+                    var count = list.length;
+                    for (var i=0; i<count; i++) {
+                        var listValue = list[i];
+                        if(typeof fieldname !== 'undefined') {
+                            listValue = list[i][fieldname];
+                        }
 
-                    if(listValue) {
-                        if(childFieldname) {
-                            if(listValue[childFieldname] == filterValue) {
-                                result.push(list[i]);
-                            }
-                        } else {
-                            if (listValue.toLowerCase().indexOf(filterValue.toLowerCase())!== -1) {
-                                result.push(list[i]);
+                        if(listValue) {
+                            if(childFieldname) {
+                                if(listValue[childFieldname] == filterValue) {
+                                    result.push(list[i]);
+                                }
+                            } else {
+                                if (listValue.toLowerCase().indexOf(filterValue.toLowerCase())!== -1) {
+                                    result.push(list[i]);
+                                }
                             }
                         }
                     }
                 }
+                return result;
             }
-            return result;
         };
     });
