706273b6db549785fa15266013a05cce10b630bb
samisalami <kontakt@samisalami.de>
2016-02-04 21:27:28 +0100

    MOD: material rarity colors

--- src/AppBundle/Resources/js/components/item/materialViewDirective.js
+++ src/AppBundle/Resources/js/components/item/materialViewDirective.js
@@ -3,11 +3,16 @@
 angular.module('app')
     .directive('materialView',['itemService','$filter', function(itemService, $filter) {
         return {
-            restrict: 'E',
+            restrict: 'EA',
             link: function($scope, $element,$attrs){
                 itemService.getMaterialsDetail(function(response){
                     $scope.materials = response;
                 });
+
+                $scope.getValidCssClass = function(string) {
+                    var regExp = new RegExp("[^A-Za-z0-9\-_]", "g");
+                    return string.replace(regExp, '').toLowerCase();
+                };
             }
         }
     }]);
