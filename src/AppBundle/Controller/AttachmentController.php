a46a8a855d5c2c68d8fe72859a241c89e3b9bfe6
samisalami <kontakt@samisalami.de>
2016-03-02 21:50:42 +0100

    MOD: refactoring attachment + renaming (1/2)

--- src/AppBundle/Controller/AttachmentController.php
+++ src/AppBundle/Controller/AttachmentController.php
@@ -9,6 +9,7 @@ use AppBundle\Service\AttachmentService;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Route;
 use AppBundle\Entity\Attachment;
+use JMS\Serializer\DeserializationContext;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
@@ -16,57 +17,45 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 
 
 class AttachmentController extends FOSRestController {
-    protected function getJMSSerializer() {
-        return $this->get('jms_serializer');
-    }
+    /**
+     * @var AttachmentService
+     */
+    private $attachmentService;
 
-    protected function deleteAttachment(Attachment $attachment) {
-        $em = $this->getDoctrine()->getManager();
-        $em->remove($attachment);
-        $em->flush();
-    }
+    public function __construct(AttachmentService $attachmentService) {
 
-    protected function addAttachment(Attachment $deserialized_attachment) {
-        $em = $this->getDoctrine()->getManager();
-        $attachment = $em->merge($deserialized_attachment);
-        $em->persist($attachment);
-        $em->flush();
-    }
-
-    protected function updateAttachment(Attachment $deserialized_attachment) {
-        $em = $this->getDoctrine()->getManager();
-        $updated_attachment = $em->merge($deserialized_attachment);
-        $em->flush();
+        $this->attachmentService = $attachmentService;
     }
 
     /**
+     * @Route("/attachment", methods={"GET"})
      * @return Response
      */
     public function getAttachmentsAction() {
         $em = $this->getDoctrine()->getManager();
         $attachments = $em->getRepository('AppBundle:Attachment')->findAll();
-        $serializer = $this->getJMSSerializer();
-        $response = new Response($serializer->serialize($attachments, 'json'));
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
+
+        $view = $this->view($attachments, 200);
+        return $this->handleView($view);
     }
 
     /**
-     * @Route("/attachment/add", methods={"POST"})
+     * @Route("api/attachment", methods={"POST"})
+     * @param Request $request
+     * @return Response
      */
     public function addAttachmentAction(Request $request) {
         $file = $request->files->get('file');
 
         if(!empty($file)) {
-            $attachment_service = new AttachmentService();
-            $attachment = $attachment_service->createAttachmentFromUpload($file);
+            $attachment = $this->attachmentService->createAttachmentFromUpload($file);
 
-            $this->addAttachment($attachment);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($attachment);
+            $em->flush();
 
-            $serializer = $this->getJMSSerializer();
-            $response = new Response($serializer->serialize($attachment, 'json'));
-            $response->headers->set('Content-Type', 'application/json');
-            return $response;
+            $view = $this->view($attachment, 200);
+            return $this->handleView($view);
         } else {
             // TODO: error handling
             return new Response(Response::HTTP_OK);
@@ -74,41 +63,43 @@ class AttachmentController extends FOSRestController {
     }
 
     /**
-     * @Route("/attachment/update", methods={"POST"})
+     * @Route("api/attachment/{id}", methods={"PUT"}, requirements={"id"="^[0-9].*$"})
      */
-    public function updateAttachmentAction(Request $request) {
-        $content = $request->getContent();
-        if(!empty($content)) {
-            $serializer = $this->getJMSSerializer();
-            $deserialized_attachment = $serializer->deserialize($content, 'AppBundle\Entity\Attachment', 'json');
-            $this->updateAttachment($deserialized_attachment);
-            $response = new Response($serializer->serialize($deserialized_attachment, 'json'));
+    public function updateAttachmentAction(Request $request, Attachment $attachment) {
+        $serializer = $this->get("jms_serializer");
+        $data = $request->getContent();
 
-            $response->headers->set('Content-Type', 'application/json');
-            return $response;
-        }
-        return new Response(Response::HTTP_OK);
+        $context = new DeserializationContext();
+        $context->setAttribute('target', $attachment);
+        $attachment = $serializer->deserialize($data, 'AppBundle\Entity\Attachment', 'json', $context);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($attachment);
+        $em->flush();
+
+        $view = $this->view($attachment, 200);
+        return $this->handleView($view);
     }
 
     /*
      * @ParamConverter("attachment", class="AppBundle:Attachment")
+     * @Route("api/attachment/{id}", methods={"DELETE"})
      */
     public function deleteAttachmentAction(Request $request, Attachment $attachment) {
-        $this->deleteAttachment($attachment);
-        $attachment_service = new AttachmentService();
-        $attachment_service->removeAttachmentFileFromServer($attachment);
-        $serializer = $this->getJMSSerializer();
-        $response = new Response($serializer->serialize($attachment, 'json'));
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($attachment);
+        $em->flush();
+
+        $this->attachmentService->removeAttachmentFileFromServer($attachment);
+
+        return new Response(Response::HTTP_OK);
     }
 
     /*
      * @ParamConverter("attachment", class="AppBundle\Entity\Attachment")
      */
     public function showAttachmentAction(Attachment $attachment) {
-        $attachment_service = new AttachmentService();
-        $attachment_file_path = $attachment_service->getFullPathToAttachmentFile($attachment);
+        $attachment_file_path = $this->attachmentService->getFullPathToAttachmentFile($attachment);
         if (file_exists($attachment_file_path)) {
             $response = new StreamedResponse();
             $response->setCallback(
@@ -122,11 +113,8 @@ class AttachmentController extends FOSRestController {
                 'Content-Type',
                 $attachment->getMimeType()
             );
-        } else {
-            //TODO: error handling
-            return new Response(Response::HTTP_OK);
+            return $response;
         }
-
-        return $response;
+        return new Response(Response::HTTP_NOT_FOUND);
     }
 }
