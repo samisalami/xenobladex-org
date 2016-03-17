f4c06bab33bbfc3a7f66ddb028bcb0116bf4125a
samisalami <kontakt@samisalami.de>
2016-03-17 22:01:32 +0100

    FIX: filter

--- src/AppBundle/Resources/js/data/mission/directive/missionViewDirective.js
+++ src/AppBundle/Resources/js/data/mission/directive/missionViewDirective.js
@@ -1,20 +1,41 @@
 'use strict';
 
 angular.module('app')
-    .directive('missionView',['MissionService','$filter', function(MissionService, $filter) {
+    .directive('missionView',['MissionService', 'PersonService', '$filter', function(MissionService, PersonService, $filter) {
         return {
             restrict: 'E',
-            link: function($scope, $element,$attrs){
+            controller: ['$scope',function($scope){
+                var that = this;
                 init();
 
                 function init() {
                     MissionService.onMissionsChanged(setMissions);
                     setMissions(MissionService.getMissions());
+                    PersonService.onPersonsChanged(setPersons);
+                    setPersons(PersonService.getPersons());
                 }
 
                 function setMissions(missions) {
-                    $scope.groupedMissions = $filter('groupByFilter')(missions,'mission_type',true);
+                    that.missions = missions;
+                    setViewData();
                 }
-            }
+
+                function setPersons(persons) {
+                    that.persons = persons;
+                    setViewData();
+                }
+
+                function setViewData() {
+                    if(that.missions && that.persons) {
+                        var missions = that.missions;
+                        missions.forEach(function(mission, index){
+                            if($.isNumeric(mission.person)) {
+                                mission.person = $filter('byId')(that.persons, mission.person) || null;
+                            }
+                        });
+                        $scope.groupedMissions = $filter('groupByFilter')(missions,'mission_type',true);
+                    }
+                }
+            }]
         }
     }]);
