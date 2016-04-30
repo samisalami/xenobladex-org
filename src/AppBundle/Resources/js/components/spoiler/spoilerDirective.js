e2ab8207873b6bf0082b62e79eeddd39100715fc
samisalami <kontakt@samisalami.de>
2016-05-01 00:11:00 +0200

    allow class

--- src/AppBundle/Resources/js/components/spoiler/spoilerDirective.js
+++ src/AppBundle/Resources/js/components/spoiler/spoilerDirective.js
@@ -3,7 +3,7 @@
 angular.module('app')
     .directive('xcxSpoiler',['$rootScope', function($rootScope) {
         return {
-            restrict: 'AE',
+            restrict: 'AEC',
             transclude: true,
             template: '<div ng-click="vm.spoiler()" class="spoiler-wrapper" ng-class="{\'active\':vm.showSpoiler}"><span class="spoiler-label"><i class="fa fa-arrow-down"></i> {{vm.spoilerLabel}}</span><div ng-if="vm.showSpoiler" ng-transclude=""></div></div>',
             controller: function ()
